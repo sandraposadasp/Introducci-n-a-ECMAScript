005b143a5c61de789dba23b3766813dd03854de3
Sandra <hi-sandra@hotmail.com>
2026-05-26 18:08:56 -0600

    funcion flecha para iniciar app

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const mostrarItinerario = () => {
 // app.js
 
 // Iniciar la aplicación
-function iniciarApp() {
+const iniciarApp = () => {
     // Ejemplo de cómo registrar destinos
     registrarDestino("Paris", "2024-06-15", "Avión");
     registrarDestino("Londres", "2024-07-01", "Tren");
